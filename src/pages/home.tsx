c76315358da787c27006844c21e5fce0b0c14bc8
Daniel Sávio <daniel.savio@treetech.com.br>
2024-02-06 17:14:27 -0300

    Feat: adding a export table to PNG

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,7 +9,11 @@ import {
 } from "../components/table";
 import { Input } from "../components/input";
 import { Label } from "../components/label";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { ReactElement } from "react";
+import icon from "../img/icon.png";
+
+import html2canvas from "html2canvas";
 
 const taxa = [4.8, 6.5, 7.5, 8, 8.8, 9.8, 10.1, 10.9, 11.7, 12.5, 13.5, 13.88];
 
@@ -17,6 +21,7 @@ export function Home() {
   const [received, setReceived]: any = useState("1000");
   const [parcelas, setParcelas]: any = useState("12");
   const [registers, setRegisters]: any = useState("");
+  const printRef= useRef(<div>i</div>);
 
   function juros(parcelas: number, recevied: number) {
     const array: any = [];
@@ -48,6 +53,10 @@ export function Home() {
           <TableCaption>Tabela de valores</TableCaption>
 
           <TableHeader>
+            <TableRow className="border-none flex w-[150px] gap-3 items-center text-green-600 font-bold hover:bg-slate-100  ">
+              <img className="h-8 " src={icon} alt="" />
+              <h1 className="">Shopping Fácil</h1>
+            </TableRow>
             <TableRow>
               <TableHead className="w-[80px]">Parcelas</TableHead>
               <TableHead className="text-center">Parcelado</TableHead>
@@ -59,7 +68,7 @@ export function Home() {
             {registers.map((register: any) => (
               <TableRow className="w-fit" key={register.parcela}>
                 <TableCell className="font-medium text-violet-700">
-                  {register.parcela}
+                  {register.parcela}x
                 </TableCell>
                 <TableCell className="text-center">
                   {register.valuePerPayent.toFixed(2)}
@@ -86,6 +95,25 @@ export function Home() {
           </TableHeader>
         </Table>
       );
+    }
+  }
+
+  async function printTable(){
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+
+    const data = canvas.toDataURL('image/jpg');
+    const link = document.createElement('a');
+
+    if (typeof link.download === 'string') {
+      link.href = data;
+      link.download = 'image.jpg';
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      window.open(data);
     }
   }
 
@@ -131,7 +159,9 @@ export function Home() {
           </div>
 
           <svg
-            onClick={()=>{window.print()}}
+            onClick={() => {
+              window.print();
+            }}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
@@ -143,9 +173,26 @@ export function Home() {
               clipRule="evenodd"
             />
           </svg>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            onClick={printTable}
+            className="w-8 h-8 text-violet-700 ml-2 p-1 border-2 rounded-full border-violet-700 cursor-pointer"
+          >
+            <path d="M12 9a3.75 3.75 0 1 0 0 7.5A3.75 3.75 0 0 0 12 9Z" />
+            <path
+              fill-rule="evenodd"
+              d="M9.344 3.071a49.52 49.52 0 0 1 5.312 0c.967.052 1.83.585 2.332 1.39l.821 1.317c.24.383.645.643 1.11.71.386.054.77.113 1.152.177 1.432.239 2.429 1.493 2.429 2.909V18a3 3 0 0 1-3 3h-15a3 3 0 0 1-3-3V9.574c0-1.416.997-2.67 2.429-2.909.382-.064.766-.123 1.151-.178a1.56 1.56 0 0 0 1.11-.71l.822-1.315a2.942 2.942 0 0 1 2.332-1.39ZM6.75 12.75a5.25 5.25 0 1 1 10.5 0 5.25 5.25 0 0 1-10.5 0Zm12-1.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+              clip-rule="evenodd"
+            />
+          </svg>
         </div>
 
-        <div id="table" >{createTable()}</div>
+        <div id="table" className="p-1 pb-2" ref={printRef}>
+          {createTable()}
+        </div>
       </div>
     </section>
   );
